50a8be9b15e7729749c6adae62381cd29f9591bd
Lukas Eisenstecken <lukas.eisenstecken.bz@gmail.com>
2025-05-11 15:07:09 +0200

    2 - exponential

--- src/simulation/lib/randVars/continous/Exponential.java
+++ src/simulation/lib/randVars/continous/Exponential.java
@@ -14,58 +14,64 @@ import simulation.lib.rng.RNG;
  * Expnential distributed random variable.
  */
 public class Exponential extends RandVar {
+	private final double lambda;
 
-	public Exponential(RNG rng) {
+	public Exponential(RNG rng, double lambda) {
 		super(rng);
+		this.lambda = lambda;
 		// TODO Auto-generated constructor stub
 	}
 
+
 	@Override
 	public double getRV() {
-		// TODO Auto-generated method stub
-		return 0;
+		double u = rng.rnd();
+		double x = (Math.log(u) / lambda);
+		return x;
 	}
+
 
 	@Override
 	public double getMean() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 1/lambda;
 	}
 
 	@Override
 	public double getVariance() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 1/Math.pow(lambda, 2);
 	}
 
 	@Override
 	public void setMean(double m) {
-		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("setMean not supported on Exponential variables");
 
 	}
 
 	@Override
 	public void setStdDeviation(double s) {
-		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("setStdDeviation not supported on Exponential variables");
+
 
 	}
 
 	@Override
 	public void setMeanAndStdDeviation(double m, double s) {
-		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("setMeanAndStdDeviation not supported on Exponential variables");
+
 
 	}
 
 	@Override
 	public String getType() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Exponential";
 	}
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
+		return String.format(
+				"Uniform distribution with parameters lambda: %.4f,\nMean: %.4f\nVariance: %.4f",
+				lambda, getMean(), getVariance()
+		);
 	}
 	
 }
