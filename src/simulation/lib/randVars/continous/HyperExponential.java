780c511e2ed4705fe8998b261fd2058e1f71018d
Lukas Eisenstecken <lukas.eisenstecken.bz@gmail.com>
2025-05-11 17:37:44 +0200

    2 - hyperexpo

--- src/simulation/lib/randVars/continous/HyperExponential.java
+++ src/simulation/lib/randVars/continous/HyperExponential.java
@@ -12,43 +12,62 @@ import static java.lang.Math.*;
  * Hyperexponential distributed random variable.
  */
 public class HyperExponential extends RandVar {
+	private double p1;
+	private double p2;
+	private double mean;
+	private double cvar;
+	private double lambda1;
+	private double lambda2;
 
-	public HyperExponential(RNG rng) {
+	public HyperExponential(RNG rng, double p1, double p2, double mean, double cvar) {
 		super(rng);
-		// TODO Auto-generated constructor stub
+		this.p1 = p1;
+		this.p2 = p2;
+		this.mean = mean;
+		this.cvar = cvar;
+		calculateLambdas();
 	}
 
 	@Override
 	public double getRV() {
-		// TODO Auto-generated method stub
-		return 0;
+		double u1 = rng.rnd();
+		double u2 = rng.rnd();
+		double lambda = this.p1 >= u1 ? lambda1 : lambda2;
+		double x = - (Math.log(u2) / lambda);
+		return x;
 	}
 
 	@Override
 	public double getMean() {
-		// TODO Auto-generated method stub
-		return 0;
+		return (p1/lambda1) + (p2/lambda2);
 	}
+
 
 	@Override
 	public double getVariance() {
-		// TODO Auto-generated method stub
-		return 0;
+		double mean = getMean();
+		// Calculation of second moment taken from https://en.wikipedia.org/wiki/Exponential_distribution
+		// E[X^n] = n!/lambda^n
+		double secondMoment = (2 * p1) / (lambda1 * lambda1) + (2 * p2) / (lambda2 * lambda2);
+		// (Chapter 1.3.4, Equation 2.21)
+		return secondMoment - mean * mean;
 	}
 
 	@Override
 	public void setMean(double m) {
-		// TODO Auto-generated method stub
+		this.mean = m;
+		calculateLambdas();
 
 	}
 
 	@Override
 	public void setStdDeviation(double s) {
-		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not supported on HyperExponential");
 
 	}
 
 	@Override
+	// TODO: This should work, but too lazy rn fix it later.
 	public void setMeanAndStdDeviation(double m, double s) {
 		// TODO Auto-generated method stub
 
@@ -56,13 +75,23 @@ public class HyperExponential extends RandVar {
 
 	@Override
 	public String getType() {
-		// TODO Auto-generated method stub
-		return null;
+		return "HyperExponential(H2)";
 	}
+
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
+		return String.format(
+				"HyperExponential (H2) Distribution\n" +
+						"p1: %.4f, p2: %.4f\n" +
+						"λ1: %.4f, λ2: %.4f\n" +
+						"Mean: %.4f, Variance: %.4f, CV: %.4f",
+				p1, p2, lambda1, lambda2, getMean(), getVariance(), cvar
+		);
+	}
+
+	private void calculateLambdas() {
+		this.lambda1 = (1/mean) * (1 + (Math.sqrt((Math.pow(cvar,2) -1 ) / Math.pow(cvar,2) + 1)));
+		this.lambda2 = (1/mean) * (1 - (Math.sqrt((Math.pow(cvar,2) -1 ) / Math.pow(cvar,2) + 1)));
 	}
 }
