2c10faedb8e9da24f67af4bfa5a65a0a804462b6
Lukas Eisenstecken <lukas.eisenstecken.bz@gmail.com>
2025-05-11 07:23:23 +0200

    initial commit

---
+++ src/study/DCCTest.java
@@ -0,0 +1,15 @@
+package study;
+
+/*
+ * TODO Problem 3.2.3 and 3.2.4 - implement this class
+ */
+public class DCCTest {
+
+    public static void main(String[] args) {
+        testDCC();
+    }
+
+    public static void testDCC() {
+        // TODO Auto-generated method stub
+    }
+}
